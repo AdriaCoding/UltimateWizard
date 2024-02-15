5c0ed89a293f4011ec0c0eae6716c7e7dcf79d96
AdriaCoding <adrialisa99@gmail.com>
2024-02-15 15:47:12 +0100

    Keys camera movement + unrefined rotations

---
+++ PlayerCamera/PlayerCamera.gd
@@ -0,0 +1,32 @@
+extends Node3D
+
+var mouse_sensitivity := 0.001
+var keys_sensitivity := 1
+
+var twist_input := 0.0
+var pitch_input := 0.0
+
+@onready var camera = $GodCamera
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input:= Vector3.ZERO
+	input.z = Input.get_axis("ui_up", "ui_down")
+	input.x = Input.get_axis("ui_left", "ui_right")
+	var base = camera.basis
+	translate(base * input * keys_sensitivity * delta)
+	camera.rotate_x(twist_input)
+	camera.rotate_y(pitch_input)
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = - event.relative.x * mouse_sensitivity
+			pitch_input = - event.relative.y * mouse_sensitivity
+
+	
